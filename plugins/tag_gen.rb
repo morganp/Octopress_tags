5ec0f318436c21605befc3375dfbdb183bec3fd6
Morgan Prior <morgan@amaras-tech.co.uk>
2013-07-12 19:12:13 +0100

    Update readme, added plugin and layouts

---
+++ plugins/tag_gen.rb
@@ -0,0 +1,64 @@
+module Jekyll
+  class TagGenerator < Generator
+    safe true
+    def generate(site)
+      if site.layouts.key? 'tag_index'
+        dir = site.config['tag_dir'] || 'tag'
+        ## Create Top level tag list /tag/index.html
+        write_all_tags_index(site, dir, site.tags.keys )
+        
+        ## Create /tag/{{tag}}/index.html
+        site.tags.keys.each do |tag|
+          write_tag_index(site, File.join(dir, tag), tag)
+        end
+      end
+    end
+  
+    def write_all_tags_index(site, dir, tags)
+      index = TagsIndex.new(site, site.source, dir, tags)
+      index.render(site.layouts, site.site_payload)
+      index.write(site.dest)
+      site.pages << index
+    end
+    
+    def write_tag_index(site, dir, tag)
+      index = TagIndex.new(site, site.source, dir, tag)
+      index.render(site.layouts, site.site_payload)
+      index.write(site.dest)
+      site.pages << index
+    end
+
+  end
+
+
+  class TagsIndex < Page
+    def initialize(site, base, dir, tags)
+      @site = site
+      @base = base
+      @dir  = dir
+      @name = 'index.html'
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tags_index.html')
+      self.data['tags']  = tags
+      self.data['title'] = "Tags"
+    end
+  end
+  
+  
+  class TagIndex < Page
+    def initialize(site, base, dir, tag)
+      @site = site
+      @base = base
+      @dir  = dir
+      @name = 'index.html'
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
+      self.data['tag'] = tag
+      tag_title_prefix = site.config['tag_title_prefix'] || 'Posts Tagged &ldquo;'
+      tag_title_suffix = site.config['tag_title_suffix'] || '&rdquo;'
+      self.data['title'] = "#{tag_title_prefix}#{tag}#{tag_title_suffix}"
+    end
+  end
+
+end
+
